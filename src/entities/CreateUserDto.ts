da75f1c575667094dfb27d2a3f373fcc1d832f0c
Alex Churkin <revisor008@gmail.com>
2019-04-19 16:35:24 +0300

    Refactoring

--- src/entities/CreateUserDto.ts
+++ src/entities/CreateUserDto.ts
@@ -1,11 +1,16 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import {IsNotEmpty, IsString} from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
-  @IsNotEmpty()
-  readonly email: string;
+    constructor(email: string, password: string) {
+        this.email = email;
+        this.password = password;
+    }
 
-  @IsString()
-  @IsNotEmpty()
-  password: string;
+    @IsString()
+    @IsNotEmpty()
+    readonly email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
 }
